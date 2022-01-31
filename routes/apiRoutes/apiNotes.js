7ac05a076d3c704dde89f592070fe1c163ac5904
mlbarre <mlbarrera1538@gmail.com>
2022-01-31 13:21:32 -0600

    added code to routes to include post and get

--- routes/apiRoutes/apiNotes.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-
-
-router.get('/api/notes')
-
-module.exports = router;
---
+++ routes/apiRoutes/notesRoutes.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+const newNoteData = require('./db/db.json')
+
+
+
+
+router.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, "./db/db.json"));
+});
+
+router.post('/api/notes', (req, res) => {
+    var addingNewNote = () => {
+    const body = body;
+    const emptyNote = [];
+
+    body.id = emptyNote[0];
+    emptyNote[0]++;
+
+    emptyNote.push(body);
+}
+    const note = addingNewNote(req.body, newNoteData);
+    res.json(note);
+});
+
+
+module.exports = router;
